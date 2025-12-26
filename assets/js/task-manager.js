49ab24a22133c56ef6a2fa9a6404ecb043d24efc
Enrique2007s <enriquesmith458@gmail.com>
2025-12-26 18:44:17 -0400

    added const and functions for task manager JS file

---
+++ assets/js/task-manager.js
@@ -0,0 +1,19 @@
+//Get elements
+const taskInput = document.getElementById('task-input');
+const addBtn = document.getElementById('addbtn');
+const taskList = document.getElementById('task-list');
+
+//load tasks from localStorage
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+//functions
+function showTasks() {
+}
+
+function addTask() {
+}
+
+function saveTasks() {
+}
+
+showTasks();
