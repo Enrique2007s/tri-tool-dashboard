29848a9dd04a2ff89a94c0be09265afc635ecedd
Enrique2007s <enriquesmith458@gmail.com>
2025-12-27 14:57:46 -0400

    added elements or constants

---
+++ assets/js/timer.js
@@ -0,0 +1,8 @@
+//get elements
+const timerElement = document.getElementById('timer');
+const startBtn = document.getElementById('startBtn');
+const pauseBtn = document.getElementById('pauseBtn');
+const resetBtn = document.getElementById('resetBtn');
+const setTimeBtn = document.getElementById('setTimeBtn');
+const minutesInput = document.getElementById('minutes');
+
